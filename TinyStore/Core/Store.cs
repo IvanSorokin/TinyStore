547af7a16fbfc0e8f194848e4cbcb0b8122e51e9
Ivan Sorokin <ivansorokin@MacBook-Pro-Ivan.local>
2018-12-21 00:35:19 +0500

    adding files

---
+++ TinyStore/Core/Store.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using TinyStore.Attributes;
+
+namespace TinyStore.Core
+{
+    public class Store
+    {
+        private readonly TinyFs fs;
+
+        public Store(string dbPath = null)
+        {
+            fs = new TinyFs(dbPath);
+        }
+
+        public void Save<T>(string id, T document, string collectionName = null)
+        {
+            collectionName = GetCollectionName(typeof(T), collectionName);
+            var json = JsonConvert.SerializeObject(document);
+            fs.SaveToCollection(json, id, collectionName);
+        }
+
+        public T FindById<T>(string id, string collectionName = null)
+        {
+            collectionName = GetCollectionName(typeof(T), collectionName);
+            var json = fs.GetFromCollection(id, collectionName);
+            return json != null ? JsonConvert.DeserializeObject<T>(json) : default(T);
+        }
+
+        public IEnumerable<T> FindByQuery<T>(Func<T, bool> selector, string collectionName = null)
+        {
+            collectionName = GetCollectionName(typeof(T), collectionName);
+            return fs.GetCollection(collectionName)
+                     .Select(x => JsonConvert.DeserializeObject<T>(x))
+                     .Where(selector);
+        }
+
+        private static string GetCollectionName(Type type, string collectionName)
+        {
+            var name = collectionName ??
+                       (Attribute.GetCustomAttribute(type,
+                                                     typeof(CollectionNameAttribute)) as CollectionNameAttribute)?.Name;
+            if (name == null)
+                throw new ArgumentException("No collection name was provided and type had no CollectionNameAttribute");
+
+            return name;
+        }
+    }
+}
---
+++ TinyStore/Core/TinyFs.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace TinyStore.Core
+{
+    internal class TinyFs
+    {
+        private readonly string rootDirectory;
+
+        public TinyFs(string dbPath)
+        {
+            rootDirectory = dbPath != null ? Path.Combine(dbPath, "tinyDb") : "tinyDb";
+            if (!Directory.Exists(rootDirectory))
+                Directory.CreateDirectory(rootDirectory);
+        }
+
+        private void EnsureCollectionFolder(string name)
+        {
+            var path = Path.Combine(rootDirectory, name);
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+        }
+
+        public bool SaveToCollection(string json, string id, string collectionName)
+        {
+            EnsureCollectionFolder(collectionName);
+            File.WriteAllText(Path.Combine(rootDirectory, collectionName, id), json);
+            return true;
+        }
+
+        public string GetFromCollection(string id, string collectionName)
+        {
+            var path = Path.Combine(rootDirectory, collectionName, id);
+            if (File.Exists(path))
+                return File.ReadAllText(path);
+            return null;
+        }
+
+        public IEnumerable<string> GetCollection(string collectionName)
+        {
+            return Directory.EnumerateFiles(Path.Combine(rootDirectory, collectionName))
+                     .Select(x => File.ReadAllText(x));
+        }
+    }
+}
